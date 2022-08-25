3ce1c046bd2c1c618e105e2dac7541bf7aa060d9
ChiaHanTu <dujiahan831@gmail.com>
2022-08-25 18:21:07 +0800

    架構調整

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -1,26 +1,17 @@
 $(document).ready(function(){
 
-	$(".menu").click(function(){ 
-    
-    $("nav").slideToggle();
-        
+	$(".mobile-nav-icon").click(function(){ 
+        $("nav").slideToggle();    
     });
     
     $(window).on('load resize',function(){
         var a_w = document.body.clientWidth;
         if(a_w >= 768) $("nav").show(); else $("nav").hide();
     });
-		
-    $("#phone").click(function(){
-
-        $(".num").slideToggle();
-
-    });
 
      //group
     $(".fancybox").fancybox({
-        openEffect  : 'none', //'elastic', 'fade' or 'none'
+        openEffect  : 'fade', //'elastic', 'fade' or 'none'
         closeEffect : 'none'
     });
-
 });
